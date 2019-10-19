23b71949482999419dab3ad9ab8763f7df953163
celeste <celiresmart@gmail.com>
2019-10-19 11:05:18 +0800

    Initial Commit

---
+++ Dollar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Dollar
+//
+//  Created by Celeste Tan on 19/10/19.
+//  Copyright © 2019 cuzwhynot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
