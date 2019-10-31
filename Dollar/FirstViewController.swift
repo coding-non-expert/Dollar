7b70b9bd0fa16438b4a96138113f5de16e84cd46
Corliss Ong <lissong2005@gmail.com>
2019-10-31 23:14:09 +0800

    Added pop up button on home screen (to provide more information) and coded the contents of pop up. Set up table view cell for daily budget on home screen. Changed the formatting of table view cell in spending screen to follow stack view.

--- Dollar/FirstViewController.swift
+++ Dollar/FirstViewController.swift
@@ -12,12 +12,24 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var homeNameLabel: UILabel!
     @IBOutlet weak var homeBalanceLabel: UILabel!
-
+    @IBOutlet weak var popUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.applyRoundCorner(popUpButton)
+        
     }
 
+    func applyRoundCorner(_ object:AnyObject){
+        object.layer.cornerRadius = object.frame.size.width / 2
+        object.layer.masksToBounds = true
+    }
 
+    @IBAction func popUpButtonPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: "Want to know how your daily budget is calculated?", message: "Dollar divides the monthly budget by the number of days in the month to give you your daily budget.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
