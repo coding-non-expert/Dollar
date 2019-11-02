d5a87d0f1036004e3e537179d71720e2bca4c51a
celeste <celiresmart@gmail.com>
2019-11-02 11:47:28 +0800

    uploaded app icon and removed all icons

--- Dollar/SavingsViewController.swift
+++ Dollar/SavingsViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class SavingsViewController: UIViewController {
     
+    let tips = [
+    "Make your own food instead of eating out",
+    "Turn off electrical appliances when not in use",
+    "Buy in bulk online to avoid unreasonable shipping fees",
+    "Always search for cheaper options elsewhere before making purchases",
+    "Two-in-one: Reduce carbon footprint and take public transport instead of investing in a car (if you have one)",
+    "USE DOLLAR!!! :DDD WHICH YOU ARE DOING NOW SO GOOD JOB",
+    "Save your coins - Literally",
+    "Avoid purchasing expensive or unnecessary items on impulse",
+    "Browse online for free, low cost, or cheaper entertainment",
+    "Master the ten-second rule in which you stop for ten seconds where you stop and ask yourself whether you really need the item you are going to buy",
+    "Designate one day a week as a “no-spend-day",
+    "Don’t hesitate to take-away what you can’t finish in restaurants, or keep your leftovers for the next day",
+    "Use less water to reduce water costs",
+    "Water any plants with reused water from washing vegetables/fruits/other items not poisonous to plants",
+    "Instead of buying new products, use everyday items that you already have (e.g. lemon and vinegar instead of laundry detergent)",
+    "Use reusable towels instead of paper napkins when you can",
+    "Sign up for every free customer rewards program you can and keep all the cards with you at all times - you never know when you need them!",
+    "Write a list when you go shopping and stick to it to avoid unplanned purchases",
+    "Repair clothing instead of tossing it - sometimes it can even become fashionable like those ripped and repaired shirts that you have to pay a thousand dollars for",
+    "Make your own when you can",
+    "Avoid stress spending and find other ways to destress",
+    "Try to buy used products when you can",
+    "Go thrift shopping!"
+    ]
+
     var savings: Int = 2
     var spendings: Int = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if savings < spendings {
+        if savings > spendings {
             performSegue(withIdentifier: "failedCheck", sender: nil)
         } else {
             performSegue(withIdentifier: "successCheck", sender: nil)
